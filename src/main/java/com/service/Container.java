875b8baefd2fa41542079ce9963ef77d805c5789
vgs <Vgs0737372373@gmail.com>
2022-08-27 12:07:39 +0300

    Update class Container

--- src/main/java/com/service/Container.java
+++ src/main/java/com/service/Container.java
@@ -1,68 +1,70 @@
 package com.service;
 
+import com.model.Auto;
+import com.model.Bus;
+import com.model.Moto;
 import com.model.Vehicle;
-import com.repository.CrudRepo;
+import com.repository.AutoRepository;
+import com.repository.BusRepiository;
+import com.repository.MotoRepository;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
-class Container{
+@Getter
+@Setter
+public class Container {
+    private final AutoService AUTO_SERVICE =
+            new AutoService(new AutoRepository());
+    private final BusService BUS_SERVICE =
+            new BusService(new BusRepiository());
+    private final MotoService MOTO_SERVICE =
+            new MotoService(new MotoRepository());
 
-//
-//    public Container(CrudRepo<T> repo) {
-//        super(repo);
-//    }
-//
-//    private T getCar(){
-//        final List<Vehicle> cars = new ArrayList<>();
-//        for(Vehicle vehicle : cars){
-//            System.out.println(vehicle.getPrice());
-//        }
-//        System.out.println(cars);
-//        return (T) cars;
-//    }
-//
-////    private <P extends Number> void getDiscount<P count>{
-////        int count;
-////        final Number discoiuntTen = 0.1;
-////        final Number discoiuntTwe = 0.2;
-////        final Number discoiuntThe = 0.3;
-////
-////        for(T car : cars){
-////            if(car.getPrice < 1){
-////                return  count = car.getPrice*discoiuntTen;
-////            }else (car.getPrice>1&& car.getPrice<5){
-////                return   count = car.getPrice*discoiuntThe;
-////            }return count = car.getPrice*discoiuntTwe;
-////
-////        }
-////    }
-//
-////
-//
-//    public static Number Discoiunt(Cont<? extends Vehicle[]> cont){
-//        Number result =0;
-//        for (Vehicle vehicle : cars){
-//            result = vehicle.getPrice();
-//        }
-//    return result;
-//    }
-//
-//    public void Discoiunt(List<Vehicle> cars){
-//        Number result;
-//        double min = 10;
-//        double max = 30;
-//        double diff = max - min;
-//        Random random = new Random();
-//        double i = random.nextDouble(diff + 1);
-//        i += min;
-//        for (Vehicle vehicle : cars){
-//            BigDecimal actual = new BigDecimal(car.getPrice(id)).multiply(i);
-//        }
-//    }
+    public Container() {
 
+    }
+
+    private List<? extends Vehicle> creareAndSave() {
+        final List<Vehicle> vehicleList = new LinkedList<>();
+        final List<Auto> autos = AUTO_SERVICE.createAndSaveAutos(5);
+        final List<Bus> buses = BUS_SERVICE.createAndSaveAutos(2);
+        final List<Moto> motos = MOTO_SERVICE.createAndSaveAutos(5);
+        for (Vehicle vehicle : autos) {
+            vehicleList.add(vehicle);
+            System.out.println(vehicle);
+        }
+        for (Vehicle vehicle : buses) {
+            vehicleList.add(vehicle);
+            System.out.println(vehicle);
+        }
+        for (Vehicle vehicle : motos) {
+            vehicleList.add(vehicle);
+            System.out.println(vehicle);
+        }
+        System.out.println("------");
+        System.out.println(vehicleList.size());
+        return vehicleList;
+    }
+
+    public void prinall() {
+        final List<? extends Vehicle> vehicleList = creareAndSave();
+        for (Vehicle el : vehicleList) {
+            System.out.println(el);
+        }
+    }
+
+    public void discount() {
+        final List<? extends Vehicle> vehicleList = creareAndSave();
+        BigDecimal newPrice;
+        for (Vehicle el : vehicleList) {
+            BigDecimal discount = BigDecimal.valueOf(0.1 + Math.random() + 0.3);
+            System.out.println("your discount - " + discount);
+            newPrice = el.getPrice().multiply(discount);
+            System.out.println("for id - " + el.getId() + " discount:" + discount + ", that`s why price for your auto - " + newPrice);
+        }
+    }
 }
